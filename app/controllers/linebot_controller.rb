70e79481a212917cdad67ed480c74939a5187e14
nemotoshogo <shota0701nemoto@gmail.com>
2020-04-11 00:05:33 +0900

    元に戻す

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,7 +1,6 @@
 class LinebotController < ApplicationController
   require 'line/bot'
 
-  # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
 
   def client
@@ -12,9 +11,6 @@ class LinebotController < ApplicationController
   end
 
   def callback
-
-    # Postモデルの中身をランダムで@postに格納する
-    @post=Post.offset( rand(Post.count) ).first
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
@@ -25,27 +21,46 @@ class LinebotController < ApplicationController
     events = client.parse_events_from(body)
 
     events.each { |event|
-
-      # event.message['text']でLINEで送られてきた文書を取得
-      if event.message['text'].include?("ジム")
-        response = "ジム行く？"
-      else
-        response = @post.name
-      end
-
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: response
-          }
-          client.reply_message(event['replyToken'], message)
+          # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
+          if event.message['text'].eql?('アンケート')
+            # private内のtemplateメソッドを呼び出します。
+            client.reply_message(event['replyToken'], template)
+          end
         end
       end
     }
 
     head :ok
+  end
+
+  private
+
+  def template
+    {
+      "type": "template",
+      "altText": "this is a confirm template",
+      "template": {
+          "type": "confirm",
+          "text": "今日のもくもく会は楽しいですか？",
+          "actions": [
+              {
+                "type": "message",
+                # Botから送られてきたメッセージに表示される文字列です。
+                "label": "楽しい",
+                # ボタンを押した時にBotに送られる文字列です。
+                "text": "楽しい"
+              },
+              {
+                "type": "message",
+                "label": "楽しくない",
+                "text": "楽しくない"
+              }
+          ]
+      }
+    }
   end
 end
