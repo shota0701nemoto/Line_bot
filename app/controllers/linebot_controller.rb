14fc178653cf2428df8abcfbcc60f972ffd30efa
nemotoshogo <shota0701nemoto@gmail.com>
2020-04-11 00:37:37 +0900

    元に戻す

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -26,7 +26,7 @@ class LinebotController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
-          if event.message['text'].eql?('アンケート')
+          if event.message['text'].eql?('ジム行く？')
             # private内のtemplateメソッドを呼び出します。
             client.reply_message(event['replyToken'], template)
           end
@@ -45,19 +45,19 @@ class LinebotController < ApplicationController
       "altText": "this is a confirm template",
       "template": {
           "type": "confirm",
-          "text": "今日のもくもく会は楽しいですか？",
+          "text": "じゃあジム行く？",
           "actions": [
               {
                 "type": "message",
                 # Botから送られてきたメッセージに表示される文字列です。
-                "label": "楽しい",
+                "label": "はい",
                 # ボタンを押した時にBotに送られる文字列です。
-                "text": "楽しい"
+                "text": "はい"
               },
               {
                 "type": "message",
-                "label": "楽しくない",
-                "text": "楽しくない"
+                "label": "はい",
+                "text": "はい"
               }
           ]
       }
